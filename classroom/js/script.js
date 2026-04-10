ea18b56a0518e00236326b718253804772a96f67
AshotSargsyan77777 <ashotsargsyan74122@gmail.com>
2026-04-10 19:32:13 +0400

    a

--- classroom/js/script.js
+++ classroom/js/script.js
@@ -1,9 +1,41 @@
-for(let i = 1; i <= 10; i++){
-    for(let j = 1; j  <= 10; j++){
-        console.log(i + " x " + j + " = " + (i * j));
+// for(let i = 1; i <= 10; i++){
+//     for(let j = 1; j  <= 10; j++){
+//         console.log(i + " x " + j + " = " + (i * j));
 
-    }
-    console.log("-----------")
+//     }
+//     console.log("-----------")
 
-}
+// }
 
+
+
+// let a = 16
+// if(a % 2 === 0){
+//     console.log("zuyg")
+
+// }
+// else{
+//     console.log("kent")
+// }
+ 
+// let delay = 0;
+
+// for (let i = 1; i < 10; i++) {
+//     for (let k = 1; k < 10; k++) {
+//         delay += 1000;
+//         setTimeout(() => {
+//             console.log(`${i} * ${k} = ${i * k}`);
+//         }, delay);
+//     }
+// }
+
+
+
+// let nerka = new Date().getFullYear;
+
+// function getAge(tvakan) {
+//     return `Du ${nerka - tvakan} tarekan es`;
+// }
+
+
+// console.log(getAge(1963));
