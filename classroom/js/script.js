c5a5cc96742fe727c3634ade26c494be1428d0d7
AshotSargsyan77777 <ashotsargsyan74122@gmail.com>
2026-04-17 19:33:49 +0400

    a

--- classroom/js/script.js
+++ classroom/js/script.js
@@ -39,3 +39,89 @@
 
 
 // console.log(getAge(1963));
+
+
+
+// function cube(num){
+//     return num ** 3
+
+// }
+
+// console.log(cube(5)); 
+
+
+// function upperCase(str){
+
+//       return str[0].toUpperCase() + str.slice(1);
+
+// }
+
+// console.log(upperCase("albert"))
+
+
+// const el = document.createElement("div")
+// con
+// document.body.appendChild(el)
+// document.body.prepend(el)
+
+
+
+const board = document.getElementById("board")
+const status = document.getElementById("status")
+
+const cells = document.querySelectorAll(".cell")
+
+
+let player = true 
+let game = true
+
+
+const state = new Array(9).fill(null)
+
+
+
+
+const wins = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,4,8],
+    [2,4,6],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+]
+
+cells.forEach((cell, i) => {
+    cell.addEventListener("click" , () =>{
+        if(!game){
+            return
+
+        }
+
+        if(cell.innerHTML){
+            return
+
+        }
+        const p = document.createElement("div")
+        p.className = player ? "x" : "o"
+        cell.appendChild(p)
+        if(checkWin(player)){
+            status.textContent = `${player ? "X" :"o"} haxtecin `
+
+        }
+        player = !player
+
+
+        
+
+    } )
+})
+
+
+function checkWin(turn){
+    return wins.some(pattern => {
+        return pattern.every(pat => state[pat] === turn )
+    })
+
+}
